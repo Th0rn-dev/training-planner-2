9ee067c94353d6b0a7ef8785910de5629415999c
th0rn <solnek33@gmail.com>
2025-10-15 22:29:14 +0300

    setted uuid type as the primary key

--- populate_data.sql
+++ populate_data.sql
@@ -1,7 +1,7 @@
-INSERT INTO public.categories (id, "name") VALUES(1, 'Соуфа');
-INSERT INTO public.categories (id, "name") VALUES(34, 'Формы');
-INSERT INTO public.categories (id, "name") VALUES(35, 'Чисао');
+INSERT INTO public.categories ("name") VALUES('Соуфа');
+INSERT INTO public.categories ("name") VALUES('Формы');
+INSERT INTO public.categories ("name") VALUES('Чисао');
 
 
-INSERT INTO public.cards (id, title, preview_image_url, video_url, category_id)
-VALUES(2, 'Название карточки', './images/test.png', './video/Соу Фа 5 размах руками.mp4', 1);
+-- INSERT INTO public.cards (id, title, category_id)
+-- VALUES(2, 'Название карточки', 1);
