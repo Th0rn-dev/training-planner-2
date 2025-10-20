97f4d2ffe163d8a161aedd18c0a9240bb7bb5c07
th0rn <solnek33@gmail.com>
2025-10-20 19:54:44 +0300

    fix connection params

--- session.py
+++ session.py
@@ -6,7 +6,7 @@ from sqlalchemy import create_engine
 from models import Base
 
 engine = create_engine(
-    f"postgresql://{os.environ.get('db_user')}:{os.environ.get('db_password')}@localhost/{os.environ.get('db_name')}")
+    f"postgresql://{os.environ.get('db_user')}:{os.environ.get('db_password')}@{os.environ.get('db_host')}/{os.environ.get('db_name')}")
 Base.metadata.create_all(engine)
 
 Session = sessionmaker(bind=engine)
